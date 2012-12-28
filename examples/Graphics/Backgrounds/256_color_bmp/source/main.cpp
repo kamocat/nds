4758f9eca819fcd6afc7aa30c613e1b829493bc4
Marshal Horn <kamocat@gmail.com>
2012-12-28 14:04:45 -0800

    palette works, but not tiling

--- examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
+++ examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
@@ -5,12 +5,12 @@
 // git adds a nice header we can include to access the data
 // this has the same name as the image
 #include "drunkenlogo.h"
-#include "sprites.h"
+#include "test.h"
 
 int main(void)
 {
     // set the mode for 2 text layers and two extended background layers
-	videoSetMode(MODE_3_2D);
+	videoSetMode(MODE_5_2D);
     vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
 	consoleDemoInit();
@@ -20,14 +20,14 @@ int main(void)
 	iprintf("\t256 color bitmap demo\n");
 	iprintf("\tNow with tiling!");
 
-	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 3,0);
 	int bg0 = bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
 
-	memcpy((void*)BG_TILE_RAM(1), spritesBitmap, 64 );	// made-up size.  Fix later.
+	memcpy((void*)BG_TILE_RAM(1), testBitmap, 50*256 );	// made-up size.  Fix later.
 
 	dmaCopy(drunkenlogoBitmap, bgGetGfxPtr(bg3), 256*256);	// put drunkenlogo as background image 3.
 	dmaCopy(drunkenlogoPal, BG_PALETTE, 256*2); // not used anymore
-	dmaCopy(spritesPal, BG_PALETTE, 18*2);	// copy the whole palette
+	//dmaCopy(testPal, BG_PALETTE, 18*2);	// replace some of the palette colors
 	
 
 	// Set a pointer to BG0's map base
@@ -37,8 +37,8 @@ int main(void)
 	 * Just for the sake of the test, we'll do them
 	 * sequentially.
 	 */
-	for( int i = 0; i < 20; i++ ) {
-		bg0map[i] = i;
+	for( int i = 0; i < 64; i++ ) {
+		bg0map[(32*24 -1) - i] = i;
 	}
 
 	while(1) {
