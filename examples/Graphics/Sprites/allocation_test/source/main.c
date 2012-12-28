aca0444f22c836fbe6cb5f11bc8bbbc0d656f67e
Marshal Horn <kamocat@gmail.com>
2012-12-27 22:47:24 -0800

    oamset only allows 128 IDs.  This example shows that the same sprite can be displayed more than once, but must have a unique ID each time.

--- examples/Graphics/Sprites/allocation_test/source/main.c
+++ examples/Graphics/Sprites/allocation_test/source/main.c
@@ -3,7 +3,7 @@
 #include <time.h> // for time()
 
 //our very simple sprite engine...
-#define SPRITE_MAX 256
+#define SPRITE_MAX 2
 
 
 // All sprites are the same size
@@ -15,7 +15,6 @@
 //to treat oam as a game object directly.
 typedef struct {
 	int x,y;
-	int dx, dy;
 	bool alive;
 	u16* gfx;
 }mySprite;
@@ -33,16 +32,14 @@ bool oom = false;	// out of memory?
 OamState *oam = &oamMain;
 
 //a sprite constructor
-void createSprite(mySprite* s, int x, int y, int dx, int dy) {
+void createSprite(mySprite* s, int x, int y ) {
 	s->alive = true;
 	s->x = x;
 	s->y = y;
-	s->dx = dx;
-	s->dy = dy;
-    
+
 	//api: allocate a chunk of sprite graphics memory
 	s->gfx = oamAllocateGfx(oam, SPRITE_SIZE, SPRITE_COLORF);
-	
+
 	allocationCount++;
 	if(s->gfx) {
 		spriteMemoryUsage += (SPRITE_SIZE & 0xFFF) << 5;
@@ -58,7 +55,7 @@ void createSprite(mySprite* s, int x, int y, int dx, int dy) {
 //sprite deconstructor
 void killSprite(mySprite *s) {
 	s->alive = false;  
- 
+
 	//api: free the graphics
 	if(s->gfx) {	
 		oamFreeGfx(oam, s->gfx);
@@ -71,25 +68,25 @@ void killSprite(mySprite *s) {
 
 //map our sprite to oam entries
 void updateSprites(void) {
-	int i;
+	int i,j;
 
 	//set oam to values required by my sprite
-	for(i = 0; i < SPRITE_MAX; i++) {
+	for(i = 0; i < 256; i+= 8) {
 		//an api function: void oamSet(OamState* oam, int id,  int x, int y, int priority, int palette_alpha, SpriteSize size, SpriteColorFormat format, const void* gfxOffset, int affineIndex, bool sizeDouble, bool hide);
 		oamSet(oam, 
-			i, 
-			sprites[i].x, sprites[i].y, 
-			0, 
-			0,
-			SPRITE_SIZE,
-			SPRITE_COLORF,
-			sprites[i].gfx, 
-			-1, 
-			false, 
-			!sprites[i].alive,
-			false,
-			false, 
-			false);
+				i, 
+				i, 8, 
+				0, 
+				0,
+				SPRITE_SIZE,
+				SPRITE_COLORF,
+				sprites[0].gfx, 
+				-1, 
+				false, 
+				!sprites[0].alive,
+				false,
+				false, 
+				false);
 	}
 }
 
@@ -97,19 +94,14 @@ void updateSprites(void) {
 void randomSprite(mySprite* s) {
 	//pick a random color index 
 	//u8 c = rand() % 2;
-	u8 c = 1;
+	u8 c = rand();
 
 	//two pixels at a time
 	u16 color = c | (c << 8);
 
 	//create a randomly oriented sprite going off in a random direction
-	createSprite(s, rand() % 256, rand() % 192, rand() % 4 - 2, rand() % 4 - 2);
+	createSprite(s, rand() % 256, rand() % 192);
 
-	//dont let sprites get stuck with 0 velocity
-	if(s->dx == 0 && s->dy == 0) {   
-		s->dx = rand() % 3 + 1;
-		s->dy = rand() % 3 + 1;
-	}
 
 	//the size (in pixels) is encoded in the low 12 bits of the Size attribute (shifted left by 5)
 	//we load new graphics each time as this is as much a test of my allocator as an example of api usage
@@ -117,21 +109,6 @@ void randomSprite(mySprite* s) {
 		swiCopy(&color, s->gfx, ((SPRITE_SIZE & 0xFFF) << 4) | COPY_MODE_FILL);
 	}	else {	
 		s->alive = false;
-	}
-}
-
-//move the sprite based on its velocity, kill it if it goes off screen and create a new random one
-void moveSprites(void) {
-	int i;
-
-	for(i = 0; i < SPRITE_MAX; i++) {
-		sprites[i].x += sprites[i].dx;
-		sprites[i].y += sprites[i].dy;
-
-		if(sprites[i].x >= 256 || sprites[i].x < 0 || sprites[i].y >= 192 || sprites[i].y < 0) {
-			killSprite(&sprites[i]);
-			randomSprite(&sprites[i]);
-		}
 	}
 }
 
@@ -149,7 +126,7 @@ int main(void)  {
 	vramSetBankD(VRAM_D_SUB_SPRITE);
 
 	consoleDemoInit();
-//	consoleDebugInit(DebugDevice_NOCASH); //send stderr to no$gba debug window
+	//	consoleDebugInit(DebugDevice_NOCASH); //send stderr to no$gba debug window
 
 	//api: initialize OAM to 1D mapping with XX byte offsets and no external palette
 	oamInit(oam, SpriteMapping_1D_128, false);
@@ -159,26 +136,25 @@ int main(void)  {
 		randomSprite(&sprites[i]);
 	//load a randomly colored palette
 	for(i = 0; i < 256; i++) {
-      SPRITE_PALETTE[i] = rand();
-      SPRITE_PALETTE_SUB[i] = rand();
+		SPRITE_PALETTE[i] = rand();
+		SPRITE_PALETTE_SUB[i] = rand();
 	}
 
 	while(1) { 
-		moveSprites();
 
 		updateSprites();
 
 		swiWaitForVBlank();
-		
+
 		//api: updates real oam memory 
 		oamUpdate(oam);
 
 		if(oom) {	
 			memUsageTemp = memUsageTemp > spriteMemoryUsage ? spriteMemoryUsage : memUsageTemp;
-    	}	
+		}	
 
 		consoleClear();
-		
+
 		printf("Memory usage: %i %i%% \n",  spriteMemoryUsage, 100 * spriteMemoryUsage / (spriteMemSize));
 		printf("Percentage fail: %i%% \n", oomCount * 100 / allocationCount);
 		printf("Lowest Usage at fail %i %i%% \n", memUsageTemp, 100 * memUsageTemp / (spriteMemSize));				
