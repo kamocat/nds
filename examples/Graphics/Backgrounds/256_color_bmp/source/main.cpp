8810c6bfe234bd524b2d5b30241e4ea7f51eb8de
Marshal Horn <kamocat@gmail.com>
2012-12-28 16:04:52 -0800

    I think this is closer to how the tile background should be configured, but now it's not doing anything.

--- examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
+++ examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
@@ -9,36 +9,34 @@
 
 int main(void)
 {
-    // set the mode for 2 text layers and two extended background layers
+	// set the mode for 2 text layers and two extended background layers
 	videoSetMode(MODE_5_2D);
-    vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
+	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
 	consoleDemoInit();
 
 	iprintf("\n\n\tHello DS devers\n");
 	iprintf("\twww.drunkencoders.com\n");
 	iprintf("\t256 color bitmap demo\n");
-	iprintf("\tNow with tiling!");
+	iprintf("\tNow with tiling!\n");
 
-	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 3,0);
-	int bg0 = bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
+	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 3, 0);
+	bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
 
-	memcpy((void*)BG_TILE_RAM(1), testBitmap, 50*256 );	// made-up size.  Fix later.
+	memcpy((void*)BG_TILE_RAM(1), testTiles, testTilesLen );
 
 	dmaCopy(drunkenlogoBitmap, bgGetGfxPtr(bg3), 256*256);	// put drunkenlogo as background image 3.
 	dmaCopy(drunkenlogoPal, BG_PALETTE, 256*2); // not used anymore
-	//dmaCopy(testPal, BG_PALETTE, 18*2);	// replace some of the palette colors
-	
+	dmaCopy(testPal, BG_PALETTE, testPalLen);	// replace some of the palette colors
+
 
 	// Set a pointer to BG0's map base
 	u16 * bg0map = (u16 *)BG_TILE_RAM(0);
 
 	/* Plot some positions on the map 
-	 * Just for the sake of the test, we'll do them
-	 * sequentially.
-	 */
-	for( int i = 0; i < 64; i++ ) {
-		bg0map[(32*24 -1) - i] = i;
+	*/
+	for( int i = 0; i < 256; ++i ) {
+		bg0map[i] = 1;
 	}
 
 	while(1) {
