f9e018e72b0e434b1367ffb29501a737b0a1fd0f
Marshal Horn <kamocat@gmail.com>
2012-12-28 23:37:21 -0800

    displays random ground tiles

--- examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
+++ examples/Graphics/Backgrounds/256_color_bmp/source/main.cpp
@@ -2,42 +2,47 @@
 
 #include <stdio.h>
 
+#include <time.h>	// for seeding rand
+
 // git adds a nice header we can include to access the data
 // this has the same name as the image
-#include "drunkenlogo.h"
-#include "test.h"
+#include "spritesheet.h"
 
 int main(void)
 {
-	// set the mode for 2 text layers and two extended background layers
-	videoSetMode(MODE_5_2D);
+	// Seed rand
+	srand(time(NULL));
+	//
+	// set the mode for 4 text layers
+	videoSetMode(MODE_0_2D);
 	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
 	consoleDemoInit();
-
-	iprintf("\n\n\tHello DS devers\n");
-	iprintf("\twww.drunkencoders.com\n");
 	iprintf("\t256 color bitmap demo\n");
 	iprintf("\tNow with tiling!\n");
 
-	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 3, 0);
-	bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
+	//bgInit(0, BgType_Text8bpp, BgSize_T_256x256, 8, 1);
+	//bgInit(1, BgType_Text8bpp, BgSize_T_256x256, 8, 1);
+	//bgInit(2, BgType_Text8bpp, BgSize_T_256x256, 10, 1);
+	bgInit(3, BgType_Text8bpp, BgSize_T_256x256, 0, 1);
 
-	memcpy((void*)BG_TILE_RAM(1), testTiles, testTilesLen );
+	memcpy((void*)BG_TILE_RAM(1), spritesheetTiles, spritesheetTilesLen );
 
-	dmaCopy(drunkenlogoBitmap, bgGetGfxPtr(bg3), 256*256);	// put drunkenlogo as background image 3.
-	dmaCopy(drunkenlogoPal, BG_PALETTE, 256*2); // not used anymore
-	dmaCopy(testPal, BG_PALETTE, testPalLen);	// replace some of the palette colors
+	dmaCopy(spritesheetPal, BG_PALETTE, spritesheetPalLen);	// set up the palette
 
 
-	// Set a pointer to BG0's map base
-	u16 * bg0map = (u16 *)BG_TILE_RAM(0);
+	// Set up pointers to the map bases
+	u16 * creatures = (u16 *)BG_TILE_RAM(8);
+	u16 * ground = (u16 *)BG_TILE_RAM(0);
 
-	/* Plot some positions on the map 
-	*/
-	for( int i = 0; i < 32; ++i ) {
-		bg0map[i] = i%3;
-		bg0map[i+32] = i%3 + 3;
+	// Put up random ground tiles
+	for( int i = 0; i < (24 * 32); ++i ) {
+		ground[i] = rand()%2;
+	}
+
+	/* random creatures array */
+	for( int i = 0; i < (24*32); ++i ) {
+		creatures[i] = 16; //rand()%6 + 32;
 	}
 
 	while(1) {
